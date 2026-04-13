a05835ec4eda79841aaa06c3ff9205112994cdf7
hyperz111 <firzeroun11@gmail.com>
2026-04-13 07:52:00 +0700

    feat: show info in center alignment

--- src/run.js
+++ src/run.js
@@ -1,5 +1,6 @@
 import { getPackageSize, parsePackageName } from "./core.js";
-import { parseArgs } from "node:util";
+import { parseArgs, styleText, stripVTControlCharacters } from "node:util";
+import { filesize } from "filesize";
 import pkg from "../package.json" with { type: "json" };
 
 /**
@@ -59,7 +60,31 @@ export const run = async (p) => {
 	try {
 		const result = await getPackageSize(name, version, (n, v) => p.stdout.write(`Fetching ${n}@${v}\n`));
 		p.stdout.write(`Succesfully fetch ${name}@${version}\n`);
-		console.log(result);
+
+		const buffers = [
+			"",
+			styleText("bold", "Name"),
+			result.name,
+			"",
+			styleText("bold", "Version"),
+			result.version,
+			"",
+			styleText("bold", "Unpacked Size"),
+			filesize(result.unpacked),
+			"",
+			styleText("bold", "Install Size"),
+			filesize(result.install),
+			"",
+			"",
+		];
+
+		p.stdout.write(
+			buffers
+				.map((line) =>
+					line ? `${" ".repeat(Math.floor((p.stdout.columns - stripVTControlCharacters(line).length) / 2))}${line}` : line,
+				)
+				.join("\n"),
+		);
 		return 0;
 	} catch (error) {
 		p.stdout.write(`Failed fetch ${name}@${version} with error: ${error.message}\n`);
